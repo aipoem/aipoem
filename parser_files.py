5033e3eb6a0ddc421a5edbe825efba30f8fbe001
Antonello Aita <antonello.aita@ibm.com>
2019-02-12 11:05:02 +0100

    parser script

---
+++ parser_files.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import numpy as np
+import io
+import re
+
+path="txt_data/" ## adapt your path 
+texts=list()
+for filename in os.listdir(path):
+    texts.append(filename)
+print('list of texts')
+print(texts)
+#### check folders
+new_folder="text_ready/"
+try:
+    os.stat(new_folder)
+except:
+    os.mkdir(new_folder)
+### 
+for f in texts:
+    print(path+f)
+    text=open(path+f,encoding="utf8")
+    lines=text.readlines()
+    print("i'm working")
+    print(f)
+    text.close()
+    #################################### work on the single file
+    new_file=open(new_folder+"ready_"+f,"w")
+    i=False ## activator
+    for l in lines:
+        if l =="ARGOMENTO.\n" or i:
+            i=True
+            if len(l)<22:
+                continue
+            if len(l)>60:
+                continue
+            elif l[0].isdigit() : ## we can improve this part with a while o something like it
+                lu=l[1:]
+                if lu[0].isdigit() :                
+                    lu=lu[1:]
+                    if lu[0].isdigit() :
+                        lu=lu[1:]               
+                new_file.write(lu) ## lu.replace('\n',"") <-- use to create and unstructured text
+            elif l[0]=="↑":
+                    continue
+            else:
+                new_file.write(l)
+    new_file.close()
+            
+    
