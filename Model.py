81e5e1dd95b05423c6d1a2b11d50e970d9903080
alessandro <alessandro.gianfelici@edison.it>
2019-02-15 15:17:06 +0100

    added the possibility to increase the number of epochs. Added some rules to the parser.

--- Model.py
+++ Model.py
@@ -1,15 +1,29 @@
-import spacy
 import logging
 import os
 import glob
 import gensim
 from multiprocessing import cpu_count
-
+import matplotlib.pyplot as plt 
 from gensim.test.utils import get_tmpfile
 from gensim.models import KeyedVectors
 
+from gensim.models.callbacks import CallbackAny2Vec
+
 logger = logging.getLogger(__name__)
 
+class TrainLogger(CallbackAny2Vec):
+    '''Callback to log information about training'''
+    def __init__(self):
+        self.epoch = 0
+        self.losses = []
+
+    def on_epoch_end(self, model):
+        self.losses.append(model.get_latest_training_loss())    
+        self.epoch += 1
+
+    def on_train_end(self, model):       
+        plt.plot(self.losses)
+        plt.show()
 
 class poema():
 
@@ -17,7 +31,12 @@ class poema():
 
         self.path_in = params['path_in']
         self.model = None
+        self._epochs = params['model_params']['epochs']
         self.model_params = params['model_params']
+
+    @property
+    def epochs(self):
+        return self._epochs
 
     def read_poem(self, text):
         f = open(os.path.join(text), "r")
@@ -25,10 +44,11 @@ class poema():
 
     def train(self):
         # At each loop, the loaded poem is appended to the string txt
+        self._losses = []
         versi = verso(self.path_in)
         self.model = gensim.models.Word2Vec(
-            versi, min_count=self.model_params["min_count"], size=self.model_params["size"], workers=cpu_count())
-        return self.model
+                versi, min_count=self.model_params["min_count"], size=self.model_params["size"], workers=cpu_count(), compute_loss=True, callbacks=[TrainLogger()])
+        return self.model.train(versi, total_examples=self.model.corpus_count, epochs=self.epochs)
 
     def save_model(self):
         self.model.save(self.model_params['filename'])
@@ -38,16 +58,16 @@ class poema():
         return self.model.wv
 
     def save_vectors(self, filename):
-        fname = get_tmpfile(filename)
+        fname=get_tmpfile(filename)
         self.model.wv.save(fname)
         return None
 
     def find_analogies(self, w1, w2, w3):
-        r = self.vectors.most_similar(positive=[w1, w3], negative=[w2])
+        r=self.vectors.most_similar(positive=[w1, w3], negative=[w2])
         print("%s - %s = %s - %s" % (w1, w2, r[0][0], w3))
 
     def nearest_neighbors(self, w):
-        r = self.vectors.most_similar(positive=[w])
+        r=self.vectors.most_similar(positive=[w])
         print(f"Neighbors of {w}:")
         for word, score in r:
             print("\t%s" % word)
@@ -56,9 +76,11 @@ class poema():
 class verso():
     def __init__(self, path):
 
-        self.path_in = path
+        self.path_in=path
 
     def __iter__(self):
         for text in glob.glob(os.path.join(self.path_in, '*.txt')):
             for line in open(text):
                 yield line.split()
+
+
--- parser_files.py
+++ parser_files.py
@@ -19,8 +19,12 @@ except:
     os.mkdir(new_folder)
 ###
 regola_e=re.compile('é')# regola per cancellare numeri nel testo
-regola_j=re.compile('j')# regola per cancellare numeri nel testo
-regola_char=re.compile("(\.|\d|\!|\?|\,|\;|\à|\’|\:|\-|\))")## regola per cancellare caratteri strani #TODO check se ci sono tutti i necessari
+regola_e2 = re.compile('ë')
+regola_j=re.compile('ï|j')# regola per cancellare numeri nel testo
+regola_a = re.compile('â')
+regola_char=re.compile("(\.|\d|\!|\?|\,|\;|\à|\:|\-|\)|\"|\,|\«|\»)")
+regola_space = re.compile("\’|\'")
+
 for f in texts:
     print(path+f)
     text = open(path+f, encoding="utf8")
@@ -28,7 +32,6 @@ for f in texts:
     print("i'm working")
     print(f)
     text.close()
-<<<<<<< HEAD
     #################################### work on the single file
     new_file=open(new_folder+"ready_"+f,"w")
     i=False ## activator
@@ -39,36 +42,7 @@ for f in texts:
             continue
         else:
             try:
-                new_file.write(regola_char.sub("",regola_e.sub("è",regola_j.sub('i',l))).lower())
-            except UnicodeEncodeError:
-                pass
-=======
-    # work on the single file
-    new_file = open(new_folder+"ready_"+f, "w")
-    i = True  # activator
-    # TODO il codice si schianta quando trova il carattere '\u2032' (è come un apostrofo ma ha un codice unicode diverso. Per non impazzire metterei un controllo che per ogni carattere non riconosciuto sostituisce uno spazio)
-    for l in lines:
-        if l == "ARGOMENTO.\n" or i:
-            i = True
-            if len(l) < 22:
-                continue
-            if len(l) > 60:
-                continue
-            elif l[0].isdigit():  # we can improve this part with a while o something like it
-                lu = l[1:]
-                if lu[0].isdigit():
-                    lu = lu[1:]
-                    if lu[0].isdigit():
-                        lu = lu[1:]
-                # lu.replace('\n',"") <-- use to create and unstructured text
-                new_file.write(lu)
-            elif l[0] == "↑":
-                continue
-            else:
-                try:
-                    new_file.write(l)
-                except UnicodeEncodeError:
-                    pass
->>>>>>> master
-
+                new_file.write(regola_char.sub("",regola_e.sub("è",regola_j.sub('i',regola_a.sub('a',regola_space.sub(" ", regola_e2.sub("e", l)) )))).lower())
+            except UnicodeEncodeError: #TODO capire esattamente quali sono i caratteri che danno errore e gestirli correttamente
+                pass        
     new_file.close()
