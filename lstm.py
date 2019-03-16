ab6a41f07487d775da3c71f8e8e073feedad4923
mattiasanvito <mattiasanvito@hotmail.it>
2019-03-16 11:46:37 +0100

    first draft lstm

---
+++ lstm.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import LSTM
+# TODO: controllare np_utils
+
